abdd0bf843612c5a2a6122b9f4ad5c3bf197ea14
leszrom <romanowski.leszek@gmail.com>
2018-12-19 18:33:06 +0100

    Mapper move form controller to service
    Additional createTask and updateTask return 'id'.
    deleteTask change to 'void'

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,8 +1,6 @@
 package com.crud.tasks.controller;
 
-import com.crud.tasks.domain.Task;
 import com.crud.tasks.domain.TaskDto;
-import com.crud.tasks.mapper.TaskMapper;
 import com.crud.tasks.service.DbService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -15,41 +13,35 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @RestController
 @RequestMapping("/v1/task")
 public class TaskController {
-    private static final String ID_DOES_NOT_EXIST = "ID doesn't exist!";
     private final DbService service;
-    private final TaskMapper taskMapper;
 
     @Autowired
-    public TaskController(DbService service, TaskMapper taskMapper) {
+    public TaskController(DbService service) {
         this.service = service;
-        this.taskMapper = taskMapper;
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "createTask", consumes = APPLICATION_JSON_VALUE)
-    public Task createTask(@RequestBody TaskDto taskDto) {
-        return service.saveTask(taskMapper.mapToTask(taskDto));
+    public Long createTask(@RequestBody TaskDto taskDto) {
+        return service.saveTask(taskDto);
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "getTasks")
     public List<TaskDto> getTasks() {
-        return taskMapper.mapToTaskDtoList(service.getAllTasks());
+        return service.getAllTasks();
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "getTask")
     public TaskDto getTask(Long id) throws TaskNotFoundException {
-        return taskMapper.mapToTaskDto(service.getTaskById(id)
-                .orElseThrow(() -> new TaskNotFoundException(ID_DOES_NOT_EXIST)));
+        return service.getTaskById(id);
     }
 
     @RequestMapping(method = RequestMethod.PUT, value = "updateTask", consumes = APPLICATION_JSON_VALUE)
-    public TaskDto updateTask(@RequestBody TaskDto taskDto) {
-        return taskMapper.mapToTaskDto(service.saveTask(taskMapper.mapToTask(taskDto)));
+    public Long updateTask(@RequestBody TaskDto taskDto) {
+        return service.saveTask(taskDto);
     }
 
     @RequestMapping(method = RequestMethod.DELETE, value = "deleteTask")
-    public TaskDto deleteTask(Long id) throws TaskNotFoundException {
-        TaskDto deletedTask = getTask(id);
+    public void deleteTask(Long id) {
         service.deleteTaskById(id);
-        return deletedTask;
     }
 }
--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -1,32 +1,39 @@
 package com.crud.tasks.service;
 
+import com.crud.tasks.controller.TaskNotFoundException;
 import com.crud.tasks.domain.Task;
+import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
 import com.crud.tasks.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class DbService {
+    private static final String ID_DOES_NOT_EXIST = "ID doesn't exist!";
     private final TaskRepository taskRepository;
+    private final TaskMapper taskMapper;
 
     @Autowired
-    public DbService(TaskRepository taskRepository) {
+    public DbService(TaskRepository taskRepository, TaskMapper taskMapper) {
         this.taskRepository = taskRepository;
+        this.taskMapper = taskMapper;
     }
 
-    public Task saveTask(final Task task) {
-        return taskRepository.save(task);
+    public Long saveTask(final TaskDto taskDto) {
+        Task task = taskMapper.mapToTask(taskDto);
+        return taskRepository.save(task).getId();
     }
 
-    public List<Task> getAllTasks() {
-        return taskRepository.findAll();
+    public List<TaskDto> getAllTasks() {
+        return taskMapper.mapToTaskDtoList(taskRepository.findAll());
     }
 
-    public Optional<Task> getTaskById(final Long id) {
-        return taskRepository.findById(id);
+    public TaskDto getTaskById(final Long id) throws TaskNotFoundException {
+        Task task = taskRepository.findById(id).orElseThrow(() -> new TaskNotFoundException(ID_DOES_NOT_EXIST));
+        return taskMapper.mapToTaskDto(task);
     }
 
     public void deleteTaskById(final Long id) {
