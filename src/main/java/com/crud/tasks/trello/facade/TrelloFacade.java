21b3c193bbc75a2a41978ae3d68a59ca1defcf60
leszrom <romanowski.leszek@gmail.com>
2019-01-11 14:43:00 +0100

    Create TrelloValidator

--- src/main/java/com/crud/tasks/trello/facade/TrelloFacade.java
+++ src/main/java/com/crud/tasks/trello/facade/TrelloFacade.java
@@ -1,36 +1,43 @@
 package com.crud.tasks.trello.facade;
 
 import com.crud.tasks.domain.TrelloBoard;
+import com.crud.tasks.domain.TrelloCard;
 import com.crud.tasks.domain.trello.TrelloBoardDto;
+import com.crud.tasks.domain.trello.TrelloCardDto;
+import com.crud.tasks.domain.trello.TrelloCardDtoRequest;
 import com.crud.tasks.mapper.TrelloMapper;
 import com.crud.tasks.service.TrelloService;
+import com.crud.tasks.trello.validator.TrelloValidator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Component
 public class TrelloFacade {
     private static final Logger LOGGER = LoggerFactory.getLogger(TrelloFacade.class);
     private TrelloService trelloService;
     private TrelloMapper trelloMapper;
+    private TrelloValidator trelloValidator;
 
     @Autowired
-    public TrelloFacade(TrelloService trelloService, TrelloMapper trelloMapper) {
+    public TrelloFacade(TrelloService trelloService, TrelloMapper trelloMapper, TrelloValidator trelloValidator) {
         this.trelloService = trelloService;
         this.trelloMapper = trelloMapper;
+        this.trelloValidator = trelloValidator;
     }
 
     public List<TrelloBoardDto> fetchTrelloBoards() {
         List<TrelloBoard> trelloBoards = trelloMapper.mapToBoards(trelloService.fetchTrelloBoards());
-        LOGGER.info("Starting filtering boards...");
-        List<TrelloBoard> filteredBoards = trelloBoards.stream()
-                .filter(board -> !board.getName().equalsIgnoreCase("test"))
-                .collect(Collectors.toList());
-        LOGGER.info("Board have been filtered. Current list size: " + filteredBoards.size());
+        List<TrelloBoard> filteredBoards = trelloValidator.validateTrelloBoards(trelloBoards);
         return trelloMapper.mapToBoardsDto(filteredBoards);
+    }
+
+    public TrelloCardDto createCard(final TrelloCardDtoRequest trelloCardDtoRequest) {
+        TrelloCard trelloCard = trelloMapper.mapToCard(trelloCardDtoRequest);
+        trelloValidator.validCard(trelloCard);
+        return trelloService.createTrelloCard(trelloMapper.mapToCardDtoRequest(trelloCard));
     }
 }
